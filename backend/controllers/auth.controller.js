f5985e92c2bfbbdb08f7c774df102a982b415faf
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-02 17:20:48 +0300

    added required fields

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,35 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
     try {
-        const { fullname, username, gender,email, password } = req.body;
+        const { fullName, username, gender,email, password,confirmPassword } = req.body;
+       if(password !== confirmPassword){
+           return res.status(400).json({
+               error: 'Passwords do not match',
+           })
+       }
+        const user = await User.findOne({username});
+
+       if(user){
+           return res.status(400).json({
+               error: 'Username already exists',
+           })
+
+           //hash the password
+           const boyProfilePic =`https://avatar.iran.liara.run/public/boy?username=${username}`
+           const girlProfilePic =`https://avatar.iran.liara.run/public/girl?username=${username}`
+           const newUser = new User({
+               fullName,
+               username,
+               email,
+               password,
+               gender,
+               profilePic:gender==='male'?boyProfilePic:girlProfilePic
+           })
+            await  newUser.save()
+           res.status(201).json({message:"User created successfully",newUser})
+       }
+
     }catch (e){
         console.log(e)
     }
