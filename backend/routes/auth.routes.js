5490ab658b27b8e0b60036ca5ac3f3397cedaeac
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-06 16:37:31 +0300

    fixed routes bug

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import { login, signup } from '../controllers/auth.controller.js';
 const router = express.Router();
 router.use(express.json());
+
+router.post("/signup",signup)
+router.post("/login",login)
 export default router;
