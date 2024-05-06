f6bbd603c4784b244f1a4291e0ae934639918930
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-06 16:35:07 +0300

    added message routes and cookiee parser

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/messages.routes.js"
 import connectToDB from "./db/connectdb.js";
+import cookieParser from "cookie-parser";
+
 const app = express()
 
 dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/users', authRoutes)
+app.use('/api/messages', messageRoutes)
+
 app.listen(port, () => {
     connectToDB()
     console.log(`Server started on port ${port}`)
+
+    
 })
