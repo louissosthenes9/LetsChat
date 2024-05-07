8d4b366995791bfc27d46596d7353fead942ecf0
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-07 19:06:13 +0300

    users modules

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSideBar = async (req, res) => {
+    try {
+      const loggedInUserId = req.user._id;
+      const filteredUsers= await User.find(
+          {_id: {$ne: loggedInUserId}}
+      ).select("-password")
+
+        return res.status(200).json(filteredUsers);
+    }catch (err){
+        console.log("error in the the getUSerForSideBar",err)
+        res.status(500).json({
+            error: "Internal Server Error",
+        })
+    }
+}
