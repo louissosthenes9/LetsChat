fc2a0bc6f4fa31dd5d10fb34e9d9a6c888f45c15
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-11 01:53:00 +0300

    new theme added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui"
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+  "./src/**/*.{html,js,jsx}",
+  './src/pages/**/*.{js,jsx}',
+  './src/components/**/*.{js,jsx}',
+  './app/**/*.{js,jsx}',
+  './src/**/*.{js,jsx}',
+],
+
+mode: 'jit',
+
+
   theme: {
     extend: {},
   },
