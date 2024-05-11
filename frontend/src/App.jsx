2d77a2e88e9fb76e0818f95f8aee9ab931f4ecd0
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-12 01:36:38 +0300

    added authentication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
-import Login from './pages/login/Login'
+//import Login from './pages/login/Login'
+import SignUp from "./pages/signup/SignUp.jsx";
 
 function App() {
 
   return (
     <>
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login/>
+      <SignUp/>
+      <p className='bg-white text-yellow-950'>Hi</p>
     </div>
       
     </>
