c0e090237b3f276ce63102f0a98330bc7a6eb9a3
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-16 02:08:38 +0300

    added functionality to the input

--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
-export default function GenderCheckBox() {
+export default function GenderCheckBox({onGenderCheckboxChange,selectedGender}) {
   return (
     <div className='flex'>
        <div className="form-control">
-           <label className='label gap-2 cursor-pointer'>
+           <label className={`label gap-2 cursor-pointer ${selectedGender==="male" ? "selected" : ""}`}>
               <span className="label-text">
                 Male
               </span>
-              <input type="checkbox" className='checkbox border-slate-900'/>
+              <input
+                  type="checkbox"
+                  onChange={()=>onGenderCheckboxChange("male")}
+                  checked={selectedGender==="male"}
+                  className='checkbox border-slate-900'/>
            </label>
        </div>
        <div className="form-control">
-           <label className='label gap-2 cursor-pointer'>
+           <label className={`label gap-2 cursor-pointer ${selectedGender==="male" ? "selected" : ""}`}>
               <span className="label-text">
                 Female
               </span>
-              <input type="checkbox" className='checkbox border-slate-900'/>
+              <input
+                  type="checkbox"
+                  onChange={()=>onGenderCheckboxChange("female")}
+                  checked={selectedGender==="female"}
+                  className='checkbox border-slate-900'/>
            </label>
        </div>
     </div>
