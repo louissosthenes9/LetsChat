53b81faccd7e12e505d82760b087b08270c5b687
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-16 02:07:36 +0300

    added navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 
 import './App.css'
 import Home from './pages/home/Home.jsx';
-//import Login from './pages/login/Login'
-//import SignUp from "./pages/signup/SignUp.jsx";
+import {Route,Routes} from "react-router-dom"
+import Login from './pages/login/Login'
+import SignUp from "./pages/signup/SignUp.jsx";
+import {Toaster} from "react-hot-toast"
 
 function App() {
 
   return (
     <>
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <SignUp/> */}
-      <Home />
-      
+          <Routes>
+           <Route  path ='/' element={<Home/>}/>
+           <Route  path ='/login' element={<Login/>}/>
+           <Route  path ='/signup' element={<SignUp />}/>
+        </Routes>
+        <Toaster/>
     </div>
       
     </>
