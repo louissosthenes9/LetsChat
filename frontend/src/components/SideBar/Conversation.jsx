660f9cab097b0ddebc4116bc502d1a1d28b6b588
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 13:58:55 +0300

    fixed alignment  bug

--- frontend/src/components/SideBar/Conversation.jsx
+++ frontend/src/components/SideBar/Conversation.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Conversation() {
   return (
-    <div>Conversation</div>
+    <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+
+      {/* Avatar goes here */}
+      <div className='avatar-online'>
+        <div className="w-12 h-12 rounded-full">
+            <img src="/vite.svg" alt="image" />
+        </div>
+      </div>
+      
+
+
+      {/* Name and mood emoji */}
+        <div className='flex gap-4 justify-between'>
+          <p className='font-bold text-white'>John Doe</p>
+          <span className='text-xl'>🙄</span>
+      </div>
+     <div className="divider my-0 py-0 h-1"  >
+
+     </div>
+    </div>
   )
 }
