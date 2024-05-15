c144c6a012fcfe1473a27a8162d1f1bd8dc2ecd6
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 13:51:55 +0300

    added  home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 
 import './App.css'
+import Home from './pages/home/Home.jsx';
 //import Login from './pages/login/Login'
-import SignUp from "./pages/signup/SignUp.jsx";
+//import SignUp from "./pages/signup/SignUp.jsx";
 
 function App() {
 
   return (
     <>
     <div className="p-4 h-screen flex items-center justify-center">
-      <SignUp/>
-      <p className='bg-white text-yellow-950'>Hi</p>
+      {/* <SignUp/> */}
+      <Home />
+      
     </div>
       
     </>
