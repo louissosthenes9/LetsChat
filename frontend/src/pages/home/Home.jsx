987deffd3c7504deb349523a14eb7f5544f97336
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 14:00:01 +0300

    fixed a typo

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideBar from '../../components/SideBar/SideBar'
+import MessageContainer from '../../components/messages/MessageContainer'
 export default function Home() {
   return (
-    <div className='clex sm:h[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
+    <div className='flex sm:h[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
     backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <SideBar />
+      <MessageContainer />
    </div>
   )
 }
