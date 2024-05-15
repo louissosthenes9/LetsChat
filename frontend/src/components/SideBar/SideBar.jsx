c5502bad618c2baf2e0a0011a0867287a28e0b3b
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 14:01:03 +0300

    refactored classes

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import SearchInput from './SearchInput'
+import Conversations from './Conversations'
+import { LogOutButton } from './LogOutButton'
 export default function SideBar() {
   return (
-    <div>
+    <div className='border-slate-500 border-r p-4 flex flex-col'>
         <SearchInput />
-        <div className='divider px-3 aria-hidden:true'></div>
+        <div className='px-3 my-4'>
+           <Conversations/>
+           <LogOutButton/>
+        </div>
     </div>
   )
 }
