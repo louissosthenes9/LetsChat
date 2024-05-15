5567c8ce52fbe93475d11f202c5763136edcc836
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 13:59:21 +0300

    logout

---
+++ frontend/src/components/SideBar/LogOutButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FaAd } from 'react-icons/fa'
+export const LogOutButton = () => {
+  return (
+    <div className='mt-auto'>
+         <FaAd className="w-6 h-6 text-white cursor-pointer" />    
+    </div>
+  )
+}
