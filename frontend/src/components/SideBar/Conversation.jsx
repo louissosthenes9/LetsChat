6c7072be1bf4f7980eae2e37a781f296a4c632cf
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-19 16:28:18 +0300

    added  backend data

--- frontend/src/components/SideBar/Conversation.jsx
+++ frontend/src/components/SideBar/Conversation.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation'
+export default function Conversation({conversation,emoji}) {
+  const {selectedConversation,setSelectedConvesation}= useConversation()
 
-export default function Conversation() {
+  const isSelected = selectedConversation?.id === conversation._id; 
+  console.log(isSelected);
+  console.log(selectedConversation);
   return (
-    <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+    <div 
+    onClick={()=>setSelectedConvesation(conversation)}
+    className={`${isSelected? bg-sky-500: ""} flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer`}>
 
       {/* Avatar goes here */}
-      <div className='avatar-online'>
+      <div className='avatar-online'> 
         <div className="w-12 h-12 rounded-full">
-            <img src="/vite.svg" alt="image" />
+            <img src={conversation.profilePic} alt="image" />
         </div>
       </div>
       
@@ -15,8 +22,8 @@ export default function Conversation() {
 
       {/* Name and mood emoji */}
         <div className='flex gap-4 justify-between'>
-          <p className='font-bold text-white'>John Doe</p>
-          <span className='text-xl'>🙄</span>
+          <p className='font-bold text-white'>{conversation.fullName}</p>
+          <span className='text-xl'>{emoji}</span>
       </div>
      <div className="divider my-0 py-0 h-1"  >
 
