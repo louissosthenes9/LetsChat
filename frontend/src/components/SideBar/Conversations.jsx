b1c1a80b3dbc6079aa21fafea1e7c6f48d0295dc
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-19 16:28:41 +0300

    a zustand error

--- frontend/src/components/SideBar/Conversations.jsx
+++ frontend/src/components/SideBar/Conversations.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Conversation from './Conversation'
-
+import useGetConversation from '../../hooks/useGetConversation'
+import {getRandomEmoji} from '../../utils/emojis'
 export default function Conversations() {
+  const {loading, conversations} = useGetConversation()
+  
   return (
     <div className="flex flex-col overflow-auto">
-       <Conversation/> 
-       <Conversation/>  
-       <Conversation/> 
-       <Conversation/> 
-       <Conversation/>  
-       <Conversation/> 
-     
+      {loading?<span className='loading loading-spinner mx-auto'></span>:null} 
+      {conversations.map((convo)=>(
+        <Conversation
+        key={convo._id}
+        conversation = {convo}
+        emoji = {getRandomEmoji()}
+        />
+
+        
+      ))}    
     </div>
   )
 }
