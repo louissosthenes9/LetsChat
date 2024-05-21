14a1d9c293e624ccd2f97f75b8b35fd99c7387ed
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-22 00:43:58 +0300

    added a  scroll bar

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import SideBar from '../../components/SideBar/SideBar'
 import MessageContainer from '../../components/messages/MessageContainer'
 export default function Home() {
   return (
-    <div className='flex sm:h[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
+    <div className='flex sm:h[450px] md:h-[550px] rounded-lg overflow-y-scroll bg-gray-400 bg-clip-padding
     backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <SideBar />
       <MessageContainer />
