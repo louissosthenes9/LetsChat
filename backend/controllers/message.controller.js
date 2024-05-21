ffd8ce5efe6ff2d701582fd1ac858242be04417b
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-21 22:23:48 +0300

    fixed the object casting bug

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,63 +1,64 @@
 import ConversationModel from "../models/conversation.model.js";
 import MessageModel from "../models/message.model.js";
 
-export const sendMessage = async  (req, res) => {
+export const sendMessage = async (req, res) => {
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id.toString();
 
-    try{
-        const {message} = req.body;
-        const {id:receiverId}= req.params;
-        const senderId = res._id;
-        let conversation=  await ConversationModel.findOneAndUpdate({
-            participants:{$all:[senderId,receiverId]},
-        })
+        // Find and update the conversation, or create it if it doesn't exist
+        let conversation = await ConversationModel.findOne({
+            participants: { $all: [senderId, receiverId] }
+        });
 
-        if(!conversation){
-            conversation = await ConversationModel.create({
-                participants:{$all:[senderId,receiverId]},
-            })
+        // If no conversation, create a new one
+        if (!conversation) {
+            conversation = new ConversationModel({
+                participants: [senderId, receiverId],
+                messages: []
+            });
         }
-
+        // Create the new message
         const newMessage = new MessageModel({
-            senderId:senderId,
-            message:message,
-            receiverId:receiverId,
+            senderId: senderId,
+            message: message,
+            receiverId: receiverId
+        });
 
-        })
+        await newMessage.save();
 
-        if(newMessage){
-            conversation.messages.push(newMessage._id)
-        }
+        // Add the new message to the conversation
+        conversation.messages.push(newMessage._id);
+        await conversation.save();
 
-        res.status(201).json(newMessage)
-    }catch(err) {
-
-        console.log("Error in sendMessage:",err)
+        res.status(201).json(newMessage);
+    } catch (err) {
+        console.log("Error in sendMessage:", err);
         res.status(400).json({
             error: "Error sending message"
         });
     }
-}
+};
 
-export const getMessages = async (req,res) =>{
+export const getMessages = async (req, res) => {
     try {
-        const {id:userToChat}= req.params;
-        const senderId = req.user._id;
+        const { id: userToChat } = req.params;
+        const senderId = req.user._id.toString();
 
         const conversation = await ConversationModel.findOne({
-            participants:{$all:[senderId,userToChat]},
+            participants: { $all: [senderId, userToChat] }
+        }).populate("messages");
 
-        }).populate("messages")
+        if (!conversation) {
+            return res.status(200).json([]);
+        }
 
-
-        res.status(200).json(conversation.messages)
-       
-        if(!conversation) return res.status(200).json([]);
-
+        res.status(200).json(conversation.messages);
     } catch (error) {
-        console.log("Error in the get messsages controller: ", error.message)
+        console.log("Error in the getMessages controller:", error.message);
         res.status(500).json({
-            error:"Internal server error"
-        })
-        
+            error: "Internal server error"
+        });
     }
 }
