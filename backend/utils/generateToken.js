b909221cd203e7620a2afa82dd774254ad29d321
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-22 14:15:39 +0300

    added token lifespan

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateTokenAndCookie = (userId,res) => {
     const token = jwt.sign({userId},process.env.SECRET,{expiresIn: '15m'})
      res.cookie('jwt', token, {
          httpOnly: true,
-         maxAge: 600000000,
+         maxAge: 60000000000000000,
          sameSite: 'strict',
          secure:process.env.NODE_ENV === 'production',
 
