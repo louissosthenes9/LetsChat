4deee91a6f48b7b8c0b17a3444b29b5d0b0e442e
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-22 14:06:16 +0300

    added a chat feature now users can converse

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,19 +1,31 @@
+import {useAuthContext} from '../../context/AuthContext'
+import useConversation from '../../zustand/store'
+export default function Message({message}) {
+  
+  const {auth} = useAuthContext()
 
+  const {selectedConversation} = useConversation()
+  
+   const fromMe = message.senderId === auth._id
+  
+  const chatClassName = fromMe ? "chat-end" : "chat-start"
+  const profilePic = fromMe ? auth.profilePic:selectedConversation?.profilePic
+  const bubbleBgColor = fromMe ? "bg-blue-500" : " "
 
-export default function Message() {
+  
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
         <div className="chat-image avatar">
              <div className="w-10 rounded-full">
-                 <img src="/vite.svg" alt="/vite.svg" />
+                 <img src={profilePic || "/vite.svg"} alt="Profile" />
              </div>
         </div>
-        <div className={`chat-bubble text-white text-bule-500`}>
-          Hi? it&amp;s louis
+        <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+          {message.message}
         </div>
         <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
 
-    </div>
+    </div> 
   )
 }
 
