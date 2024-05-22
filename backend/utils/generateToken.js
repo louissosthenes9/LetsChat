d05b707b256ceea54818fa59662b6d7e3f955019
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-22 15:22:58 +0300

    fixed the expiry date error

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken'
 
 const generateTokenAndCookie = (userId,res) => {
-    const token = jwt.sign({userId},process.env.SECRET,{expiresIn: '15m'})
+    const token = jwt.sign({userId},process.env.SECRET,{expiresIn: '59m'})
      res.cookie('jwt', token, {
          httpOnly: true,
-         maxAge: 60000000000000000,
+         maxAge: 59*60*1000,
          sameSite: 'strict',
          secure:process.env.NODE_ENV === 'production',
 
