b4fcba6be9c5479b39104528ec92b1484720750f
Louissosthenes9 <louissosthenes9@gmail.com>
2024-05-24 11:47:05 +0300

    added socket io context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import {AuthContextProvider} from "./context/AuthContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from './context/SocketContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-        <BrowserRouter>
-            <AuthContextProvider>
-                <App/>
-            </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
 
-        </BrowserRouter>
-     
+      </AuthContextProvider>
+
+    </BrowserRouter>
+
   </React.StrictMode>
 )
